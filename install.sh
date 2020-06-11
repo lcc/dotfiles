2986a2e1872202509176f225e85e6acb26a10339
Lucas Cabral <lcc2@cesar.org.br>
2020-06-11 15:32:51 -0300

    Add doom emacs dependency

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)7"
 pacman -S --needed - < pkglist.txt
-
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install # M-x all-the-icons-install-fonts(might be necessary)
