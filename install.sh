155bbfdb06f0485d3a501b7973a9c6d3af082df8
Lucas Cabral <lcc2@cesar.org.br>
2020-07-28 09:33:17 -0300

    Minor improvements

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 pacman -S --needed - < pkglist.txt
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install # M-x all-the-icons-install-fonts(might be necessary)
+
+mkdir -p temp/
+cd temp
+wget https://support.steampowered.com/downloads/1974-YFKL-4947/SteamFonts.zip
+unzip SteamFonts.zip && rm SteamFonts.zip
+mkdir -p ~/.fonts
+mv * ~/.fonts
+cd ..
+rm -r temp/
