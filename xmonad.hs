46eb07bbeffe1961dc75e6c2e04007504ff794bc
Lucas Cabral <lucascostacabra@gmail.com>
2021-05-20 07:58:16 -0300

    Add xmonad xmessage to default keybindings

--- xmonad.hs
+++ xmonad.hs
@@ -123,6 +123,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+
+    -- Run xmessage with a summary of the default keybindings (useful for beginners)
+    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ]
     ++
 
